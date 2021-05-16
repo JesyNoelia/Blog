db917ca406051c1f85711f56b75bd93a8ae78245
Jesy <jncepero@gmail.com>
2021-05-16 23:23:27 +0200

    Maquetación de secciones del Blog y creación del formulario

--- AngularBlog/src/app/services/blog.service.ts
+++ AngularBlog/src/app/services/blog.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Post } from '../interfaces/post.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
 
-  constructor() { }
+
+  constructor(private httpClient: HttpClient) { }
+
+  agregarPost() {
+
+  }
+
+  getAllPosts() {
+    return
+  }
+
+  getPostsByCategoria() {
+
+  }
 }
