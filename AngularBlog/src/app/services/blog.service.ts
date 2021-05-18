a0aac118ef18f45fab3332b7ada89ab5a9b8f693
Jesy <jncepero@gmail.com>
2021-05-19 00:22:54 +0200

    método getByCategory

--- AngularBlog/src/app/services/blog.service.ts
+++ AngularBlog/src/app/services/blog.service.ts
@@ -12,13 +12,31 @@ export class BlogService {
   constructor() {
     this.arrayPost = [{
 
-      titulo: 'Atardecer en la playa',
-      texto: 'Juegos en una tarde de playa',
+      titulo: 'Stunning Beach sunset',
+      texto: "To scape and sit quietly on the beach, that's an idea of paradise",
       autor: 'J-C-P',
-      imagen: 'https://neliosoftware.com/es/wp-content/uploads/sites/3/2018/07/aziz-acharki-549137-unsplash-1200x775.jpg',
+      imagen: 'https://images.unsplash.com/photo-1529348915581-73628f0cf212?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmVhY2glMjBzdW5zZXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80',
       fecha: '17/05/2021',
       categoria: 'Travel',
-    }
+    },
+    {
+      titulo: 'Best foldable phones 2021',
+      texto: "The Samsung Galaxy Z Fold 2, for instance, costs twice as much as the company's 2020 flagship Samsung Galaxy S20. That's what you'll need to expect if you want to enjoy a device that combines the portability of a phone with the advantage of unfolding inner displays that rival tablets when it comes to screen real estate. ",
+      autor: 'J-C-P',
+      imagen: 'https://cdn.pocket-lint.com/r/s/1200x/assets/images/148375-phones-feature-foldable-phones-image1-158skhnyrl.jpg',
+      fecha: '21/04/2021',
+      categoria: 'Technology',
+    },
+
+    {
+      titulo: 'Why yoga is important in our daily life',
+      texto: "Yoga helps in increasing our body awareness, helps in relieving stress, helps in reducing muscle tension, strain as well as inflammation, helps in building attention and concentration, calms our nervous system.",
+      autor: 'J-C-P',
+      imagen: 'https://lh3.googleusercontent.com/proxy/0D80Fdm8T5hSB4wQH_5i2pzqvG0LoMdAzPLYbyMckMrrhcG6q2HyYY2m72CbldDHQfNZo4U6WuKjkwi_JmF6EmTIxua06eQCs3DglnzWVWcohKtZyph7G_U',
+      fecha: '02/03/2021',
+      categoria: 'Health',
+    },
+
     ];
   }
 
@@ -30,8 +48,11 @@ export class BlogService {
     return this.arrayPost
   }
 
-  getPostsByCategoria() {
-    return this.arrayPost
+  getPostsByCategoria(pCategoria: string): Post[] {
+    const resultado = this.arrayPost.filter(post => { post.categoria === pCategoria })
+    console.log(resultado);
+    return resultado
+
 
   }
 }
