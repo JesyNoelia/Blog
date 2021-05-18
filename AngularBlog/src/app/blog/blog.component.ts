40df1dee24bf887ed291bd8a86edacc6b5dcc4b2
Jesy <jncepero@gmail.com>
2021-05-19 00:24:02 +0200

    creacion de ventos para filtrar los post por categorias
    creacion de eventos

--- AngularBlog/src/app/blog/blog.component.ts
+++ AngularBlog/src/app/blog/blog.component.ts
@@ -8,13 +8,26 @@ import { BlogService } from '../services/blog.service';
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent implements OnInit {
+
   posts: Post[];
 
   constructor(private blogService: BlogService) { }
 
   ngOnInit() {
     this.posts = this.blogService.getAllPosts()
+    console.log(this.posts);
 
+
+  }
+
+  onClick($event) {
+    this.posts = this.blogService.getPostsByCategoria($event.target.innerText);
+    console.log($event.target.innerText);
+    return this.posts;
+
+  }
+  onClickAll() {
+    this.posts = this.blogService.getAllPosts();
   }
 
 }
