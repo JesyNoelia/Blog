fcd4a7359ba16fb08e844eaa872ef9bd943a2561
Jesy <jncepero@gmail.com>
2021-05-19 21:15:22 +0200

    Creación de archivo con datos base para crear el blog

---
+++ AngularBlog/src/db/posts.db.ts
@@ -0,0 +1,80 @@
+import { Post } from "src/app/interfaces/post.interface";
+
+
+export const POSTS: Post[] = [
+    {
+
+        titulo: 'Stunning Beach sunset',
+        texto: "To scape and sit quietly on the beach, that's an idea of paradise",
+        autor: 'J-C-P',
+        imagen: 'https://images.unsplash.com/photo-1529348915581-73628f0cf212?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmVhY2glMjBzdW5zZXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80',
+        fecha: '17/05/2021',
+        categoria: 'Travel',
+    },
+    {
+        titulo: 'Best foldable phones 2021',
+        texto: "The Samsung Galaxy Z Fold 2, for instance, costs twice as much as the company's 2020 flagship Samsung Galaxy S20. That's what you'll need to expect if you want to enjoy a device that combines the portability of a phone with the advantage of unfolding inner displays that rival tablets when it comes to screen real estate. ",
+        autor: 'J-C-P',
+        imagen: 'https://cdn.pocket-lint.com/r/s/1200x/assets/images/148375-phones-feature-foldable-phones-image1-158skhnyrl.jpg',
+        fecha: '21/04/2021',
+        categoria: 'Technology',
+    },
+
+    {
+        titulo: 'Why yoga is important in our daily life',
+        texto: "Yoga helps in increasing our body awareness, helps in relieving stress, helps in reducing muscle tension, strain as well as inflammation, helps in building attention and concentration, calms our nervous system.",
+        autor: 'J-C-P',
+        imagen: 'https://www.hola.com/imagenes/estar-bien/20210125183194/yoga-para-pricipiantes-posturas-basicas-faciles/0-913-85/yogacuello-m.jpg',
+        fecha: '02/03/2021',
+        categoria: 'Health',
+    },
+    {
+        titulo: 'Extraordinary Glass Objects',
+        texto: "Researchers in France have developed a new laser-based approach for 3D-printing complex glass objects, something that previously has been achieved only through a more typical layer-by-layer additive manufacturing (AD) technique.",
+        autor: 'J-C-P',
+        imagen: 'https://www.designnews.com/sites/designnews.com/files/styles/article_featured_retina/public/AdobeStock_59665824.jpg?itok=uJF9EjWt',
+        fecha: '02/03/2021',
+        categoria: 'Design',
+    },
+    {
+        titulo: 'Japan’s Olympic-Sized Problem',
+        texto: "Many Japanese citizens oppose the government’s plan to hold the Tokyo Olympics despite a coronavirus surge and lagging immunization rates.",
+        autor: 'J-C-P',
+        imagen: 'https://acf.geeknetic.es/imgw/imagenes/noticias/2019/16343-jjoo-esports-1.jpg?f=webp',
+        fecha: '02/03/2021',
+        categoria: 'Culture',
+    },
+    {
+        titulo: 'SoftBank Sells Renewable-Energy Unit in India',
+        texto: "SoftBank Group Corp. is selling its Delhi-based renewable-energy unit to an Indian rival, a step that effectively pulls the plug on what was once the world’s most ambitious solar investor.",
+        autor: 'J-C-P',
+        imagen: 'https://s03.s3c.es/imag/_v0/770x420/1/b/e/700x420_softbank-coronavirus.jpg',
+        fecha: '02/03/2021',
+        categoria: 'Business',
+    },
+    {
+        titulo: 'Supreme Court ruling leaves Madrid Central low-emission scheme hanging in the balance',
+        texto: "Spain’s top judges have struck down an appeal to save the anti-pollution plan, which the current mayor wants to replace with a new system. Until then, it is unclear whether fines will continue to be enforced",
+        autor: 'J-C-P',
+        imagen: 'https://images.english.elpais.com/resizer/0PIG1JuHeKPn4eqncrqWiRewF8s=/828x0/filters:focal(3866x2663:3876x2673)/cloudfront-eu-central-1.images.arcpublishing.com/prisa/VULZ5VBZTJAUNHMET7MI7VPNLU.jpg',
+        fecha: '02/03/2021',
+        categoria: 'Politics',
+    },
+
+    {
+        titulo: 'Science drill ship sets depth record off Japan',
+        texto: "Researchers working off the coast of Japan lowered a giant piston corer through more than 8km of water in order to pull sediments from the seabed. It took two hours and 40 minutes simply for the equipment to descend into position above the ocean floor.The drilling exercise, mounted from the Research Vessel Kaimei, is part of a project to read the history of great earthquakes in the region.",
+        autor: 'J-C-P',
+        imagen: 'https://ichef.bbci.co.uk/news/976/cpsprodpb/527D/production/_118571112_kaimei04-1.jpg',
+        fecha: '02/03/2021',
+        categoria: 'Science',
+    },
+    {
+        titulo: 'Bitcoin dives as China cracks down on crypto',
+        texto: "The price of Bitcoin fell below $34,000 (£24,030) for the first time in three months on Wednesday, after China imposed fresh curbs on crypto-currencies.",
+        autor: 'J-C-P',
+        imagen: 'https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/53F8/production/_118569412_bitcoin2.jpg',
+        fecha: '02/03/2021',
+        categoria: 'World',
+    },
+]
