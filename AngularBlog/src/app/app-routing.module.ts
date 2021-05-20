37f682377fa780a54b1d6e79d3736a5054889fca
Jesy <jncepero@gmail.com>
2021-05-20 20:39:16 +0200

    incorporacion de nuevo componente en los diferentes archivos involucrados

--- AngularBlog/src/app/app-routing.module.ts
+++ AngularBlog/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlogComponent } from './blog/blog.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { PostComponent } from './post/post.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/blog' },
   { path: 'blog', component: BlogComponent },
   { path: 'new', component: FormularioComponent },
+  { path: ':categoria/:idPost', component: PostComponent },
   { path: '**', redirectTo: '/blog' }
 ];
 
--- AngularBlog/src/app/app.module.ts
+++ AngularBlog/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { BlogComponent } from './blog/blog.component';
 import { FormularioComponent } from './formulario/formulario.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlogComponent,
-    FormularioComponent
+    FormularioComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
